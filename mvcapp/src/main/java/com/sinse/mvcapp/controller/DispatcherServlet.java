6ac336e949650a3360f8b38b8f9606692a2e7646
lecturepc <devlearncampus@gmail.com>
2025-07-09 15:14:17 +0900

    금일수업

--- mvcapp/src/main/java/com/sinse/mvcapp/controller/DispatcherServlet.java
+++ mvcapp/src/main/java/com/sinse/mvcapp/controller/DispatcherServlet.java
@@ -1,7 +1,9 @@
 package com.sinse.mvcapp.controller;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Properties;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
@@ -22,15 +24,25 @@ public class DispatcherServlet extends HttpServlet{
 	//설정파일로 부터 , 해당 요청에 어떤 하위 컨트롤러가 작동해야 하는지 분석하여, 
 	//요청을 전달하기 위해
 	FileInputStream fis;
+	Properties props; // java.util.Map의 자식 중 Properties 라는 객체는 자신이 읽어들인 문자열이 만일 
+								//   = 을 기준으로 key=value 형태로 되어 잇다면, 이것을 해석할 수 있는 객체임
 	
-	public void init(ServletConfig config) throws ServletException {
+	public void init(ServletConfig config) throws ServletException{
 		ServletContext context=config.getServletContext(); //현재 서블릿이 실행되고 문맥상의 애플리케이션..
 		//WEB-INF:외부 웹브라우저에서의 접근이 불가능한 보안 디렉토리이지만, 자바코드내부에서는 얼마든지 접근가능
 		String realPath=context.getRealPath(config.getInitParameter("contextConfigLocation"));
 		
 		logger.debug(realPath);
 		
-		//fis = new FileInputStream("C:\\lecture_workspace\\backend\\javaee_workspace\\mvcapp\\src\\main\\webapp\\WEB-INF\\servlet-mapping.data");
+		try {
+			fis = new FileInputStream(realPath);
+			props = new Properties();
+			props.load(fis); //실제적으로 파일을 읽어들인 객체는 fis이므로, props fis를 로드해야 한다..
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
@@ -48,14 +60,23 @@ public class DispatcherServlet extends HttpServlet{
 		 * 진입점이 되는 클래스가 매 요청마다 1:1 대응되는 if조건문이 작성되고 있음
 		 * */
 		Controller controller=null;
-		if(request.getRequestURI().equals("/blood.do")) {//클라이언트의 요청이 혈액형이면..
-			//혈액형을 전문적으로 처리하는 컨트롤러에게 업무 분담!!!
-			//요청에 대한 처리를 1:1 대응하는 객체로 처리하는 개발 패턴을 가리켜 Command Pattern 
-			controller = new BloodController();			
-		}else if(request.getRequestURI().equals("/color.do")) {
-			controller = new ColorController();
+		//혈액형을 전문적으로 처리하는 컨트롤러에게 업무 분담!!!
+		//요청에 대한 처리를 1:1 대응하는 객체로 처리하는 개발 패턴을 가리켜 Command Pattern 
+		//controller = new BloodController();
+		//logger.debug(props.getProperty("/blood.do"));
+		//com.sinse.mvcapp.controller.BloodController
+		//new 연산자 만이 인스턴스를 생성할수 있는 건 아니다!!!
+		try {
+			Class clazz=Class.forName(props.getProperty(request.getRequestURI()));
+			controller=(Controller)clazz.newInstance();
+			controller.execute(request, response); //컨트롤러 실행
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
 		}
-		controller.execute(request, response);
 		
 	}
 	
