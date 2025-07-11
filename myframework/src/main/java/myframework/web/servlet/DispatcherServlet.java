a0d251c35b6b51da743024200b0344f235afc532
lecturepc <devlearncampus@gmail.com>
2025-07-11 10:27:37 +0900

    금일수업'

--- myframework/src/main/java/myframework/web/servlet/DispatcherServlet.java
+++ myframework/src/main/java/myframework/web/servlet/DispatcherServlet.java
@@ -1,5 +1,32 @@
 package myframework.web.servlet;
 
-public class DispatcherServlet {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//웹애플리케이션의 모든 요청을 1차적으로 처리하는 전면 컨트롤러 
+public class DispatcherServlet extends HttpServlet{
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+	
+	protected void doRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
+	
 }
+
+
+
+
+
+
+
+
