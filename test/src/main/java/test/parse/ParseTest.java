b4c0855433ab37fad5f426e07aa06ce5698d8be9
lecturepc <devlearncampus@gmail.com>
2025-07-11 09:57:25 +0900

    금일수업'

--- test/src/main/java/test/parse/ParseTest.java
+++ test/src/main/java/test/parse/ParseTest.java
@@ -1,18 +1,35 @@
 package test.parse;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.net.URISyntaxException;
 import java.net.URL;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 public class ParseTest {
 
 	public ParseTest() {
 		URL url=this.getClass().getClassLoader().getResource("test/parse/servlet-mapping.json");
-		try {
-			File file= new File(url.toURI());
-			System.out.println(file.getAbsolutePath());
+		try(Reader reader = new FileReader(new File(url.toURI()))) {
+			
+			Gson gson = new Gson();
+			
+			JsonElement root=JsonParser.parseReader(reader);
+			System.out.println(root);
+			
+			
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
 		}
 		
 	}
